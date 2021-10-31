cf013e72fc93f317cd9c658598af4c26d82cf399
ItzBraveLion <himanshuupadhyay9457@gmail.com>
2021-10-31 12:18:30 +0530

    Sevelete

---
+++ myproject/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	let name="Himanshu";
+	let FavColor="Yellow";
+
+	const handleClick=()=>{
+		FavColor="Yellow";
+	}
+	// const handleInput=(e)=>{
+	// 	FavColor=e.target.value;
+	// }
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p style="color: {FavColor}">{FavColor}</p>
+	<button on:click={handleClick}>Update Fav Color</button>
+	<!-- For single way binding remove value -->
+	<!-- <input type="text" on:input={handleInput} value={FavColor}> -->
+
+	<input type="text" bind:value={FavColor}>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
